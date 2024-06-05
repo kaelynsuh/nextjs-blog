d9fd54c5b63c8cd427c82e1fe9701057ffa1c47d
Kaelyn Suh <kaelyn.suh@gmail.com>
2024-06-05 16:41:17 -0700

    twitter test

--- pages/layout.js
+++ pages/layout.js
@@ -13,6 +13,18 @@ export default function Layout() {
           content="https://www.lilsnack.co/assets/home-thumbnail.jpg"
         />
         <meta property="og:image:type" content="image/png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:site" content="@lilsnackdaily" />
+        <meta property="twitter:title" content="Lil Snack" />
+        <meta
+          property="twitter:description"
+          content="New pop-culture games & puzzles every day!"
+        />
+        <meta
+          property="twitter:image"
+          content="https://www.lilsnack.co/lil-snack-twitter-card.jpg"
+        />
       </Head>
       <h2>HELLO</h2>
     </>
