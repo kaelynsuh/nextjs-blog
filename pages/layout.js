d756c12a736468e476d78e894d76257c0f54873e
Kaelyn Suh <kaelyn.suh@gmail.com>
2024-06-05 16:44:30 -0700

    surely

--- pages/layout.js
+++ pages/layout.js
@@ -4,13 +4,41 @@ export default function Layout() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content="Next.js blog" />
-        <meta property="og:description" content="Next.js blog description" />
+        <title>Lil Snack</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <script async src="https://tally.so/widgets/embed.js"></script>
+
+        <meta
+          name="description"
+          content="New pop-culture games & puzzles every day!"
+        />
+
+        <meta property="og:title" content="Lil Snack" />
+        <meta property="og:url" content="https://www.lilsnack.co/" />
+        <meta
+          property="og:description"
+          content="New pop-culture games & puzzles every day!"
+        />
         <meta
           property="og:image"
-          content="https://www.lilsnack.co/assets/home-thumbnail.jpg"
+          content="https://www.lilsnack.co/assets/home-thumbnail.png"
         />
         <meta property="og:image:type" content="image/png" />
 
@@ -25,6 +53,30 @@ export default function Layout() {
           property="twitter:image"
           content="https://www.lilsnack.co/lil-snack-twitter-card.jpg"
         />
+
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-2477754794012773"
+        ></meta>
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+						!function(f,b,e,v,n,t,s)
+						{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+						n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+						if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+						n.queue=[];t=b.createElement(e);t.async=!0;
+						t.src=v;s=b.getElementsByTagName(e)[0];
+						s.parentNode.insertBefore(t,s)}(window,document,'script',
+						'https://connect.facebook.net/en_US/fbevents.js');
+						fbq('init', '1784913425673905'); 
+						fbq('track', 'PageView');
+					`,
+          }}
+        />
+        {/* End Meta Pixel Code */}
       </Head>
       <h2>HELLO</h2>
     </>
